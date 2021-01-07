c96d1427c05df1d0798b62ce1d5d790dc576b461
Jonathan Alexander <me@jmalexan.com>
2021-01-07 15:04:41 -0800

    Upload current WIP

---
+++ Assets/Scripts/CarChanger.cs
@@ -0,0 +1,28 @@
+using UnityEngine;
+
+public class CarChanger : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.touchCount > 0)
+        {
+            if (Input.GetTouch(0).phase == TouchPhase.Began)
+            {
+                Ray ray = Camera.main.ScreenPointToRay(Input.GetTouch(0).position);
+                RaycastHit hit;
+                if (Physics.Raycast(ray, out hit))
+                {
+                    hit.collider.gameObject.SetActive(false);
+                }
+
+            }
+        }
+    }
+}
---
+++ Assets/Scripts/CarPlacerPlane.cs
@@ -0,0 +1,48 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.XR.ARFoundation;
+
+public class CarPlacerPlane : MonoBehaviour
+{
+
+    private bool hasTouched = false;
+
+    public GameObject prefab = null;
+
+    private GameObject display = null;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        ARRaycastManager raycaster = gameObject.GetComponent<ARRaycastManager>();
+        if (Input.touchCount > 0 && !hasTouched)
+        {
+            if (Input.GetTouch(0).phase == TouchPhase.Began)
+            {
+                hasTouched = true;
+                List<ARRaycastHit> hits = new List<ARRaycastHit>();
+
+                raycaster.Raycast(Input.GetTouch(0).position, hits, UnityEngine.XR.ARSubsystems.TrackableType.PlaneWithinBounds);
+
+                if (this.display == null)
+                {
+                    this.display = Instantiate(prefab, hits[0].pose.position, hits[0].pose.rotation);
+                }
+                else
+                {
+                    this.display.transform.position = hits[0].pose.position;
+                    this.display.transform.rotation = hits[0].pose.rotation;
+                }
+            }
+
+        }
+
+    }
+}
